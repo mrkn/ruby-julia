b852a9b0edb6689e358888107f0688b34b95c924
Kenta Murata <mrkn@mrkn.jp>
2018-08-29 13:15:34 +0900

    Make spec success

--- lib/julia.rb
+++ lib/julia.rb
@@ -2,4 +2,11 @@ module Julia
   require 'julia/version'
   require 'julia/libjulia'
   require 'julia/init'
+
+  module_function
+
+  def eval(str)
+    Julia.init unless LibJulia.respond_to? :jl_eval_string
+    LibJulia.jl_eval_string(str)
+  end
 end

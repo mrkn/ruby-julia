aa763db7bb3fda380e3c2de147a91118ec3c71f7
Kenta Murata <mrkn@mrkn.jp>
2018-08-29 11:20:13 +0900

    Modify find_libjulia to use investigator

--- lib/julia/libjulia/finder.rb
+++ lib/julia/libjulia/finder.rb
@@ -1,3 +1,6 @@
+require 'julia/error'
+require 'fiddle'
+
 module Julia
   module LibJulia
     module Finder
@@ -15,32 +18,84 @@ module Julia
       LIBPREFIX = libprefix || 'lib'
       LIBSUFFIX = libsuffix || 'so'
 
-      module_function
+      class << self
+        DEFAULT_JULIA = -'julia'
 
-      def find_libjulia
-        top_dir = File.expand_path('../../../..', __FILE__)
-        lib_dir = File.join(top_dir, 'lib')
-        dll_path = [lib_dir]
-        if RUBY_PLATFORM =~ /(?:mingw|mswin|cygwin)/i
-          ENV['PATH'].split(';').each do |path|
-            dll_path << path.strip
+        def find_libjulia(julia = nil)
+          debug_report "find_libjulia(#{julia.inspect})"
+          julia_cmd, julia_config = investigate_julia(julia)
+
+          libpath = File.join(julia_config[:libdir], "libjulia.#{LIBSUFFIX}")
+          if File.file? libpath
+            begin
+              return dlopen(libpath)
+            rescue Fiddle::DLError
+              debug_report "#{$!.class}: #{$!.message}"
+            else
+              debug_report "Success to dlopen #{fullname}"
+            end
+          else
+            debug_report "Unable to find #{fullname}"
           end
-          ENV['PATH'] = "#{lib_dir};#{ENV['PATH']}"
+
+          raise Julia::JuliaNotFound
         end
-        if RUBY_PLATFORM !~ /(?:mswin|darwin)/i
-          if ENV['LD_LIBRARY_PATH']
-            ENV['LD_LIBRARY_PATH'].split(':').each do |path|
-              dll_path << path.strip
+
+        def investigate_julia(julia = nil)
+          julia ||= DEFAULT_JULIA
+          Array(julia).each do |julia_cmd|
+            julia_config = run_julia_investigator(julia_cmd)
+            return [julia_cmd, julia_config] unless julia_config.empty?
+          end
+        rescue
+          debug_report "investigate_julia: (#{$!.class}) #{$!.message}"
+          raise Julia::JuliaNotFound
+        else
+          raise Julia::JuliaNotFound
+        end
+
+        def run_julia_investigator(julia_cmd)
+          debug_report "run_julia_investigator(#{julia_cmd})"
+          IO.popen({}, [julia_cmd, julia_investigator_jl], 'r') do |io|
+            {}.tap do |config|
+              io.each_line do |line|
+                next unless line =~ /: /
+                key, value = line.chomp.split(': ', 2)
+                case value
+                when 'true', 'false'
+                  value = (value == 'true')
+                end
+                config[key.to_sym] = value if value != 'nothing'
+              end
             end
           end
+        rescue Errno::ENOENT
+          raise Julia::JuliaNotFound
         end
-        dll_path.map! {|path| File.join(path, "#{LIBPREFIX}julia.#{LIBSUFFIX}") }
-        dll_path << ENV['LIBJULIA'] if ENV['LIBJULIA']
-        lib_path = dll_path.select {|path| File.file?(path) }
-        if lib_path.empty?
-          raise "Unable to find Julia shared library.  The list of candidates:\n#{lib_path.join("\n")}"
+
+        def julia_investigator_jl
+          File.expand_path('../investigator.jl', __FILE__)
         end
-        lib_path
+
+        private
+
+        def dlopen(path)
+          # NOTE: libjulia needs to be dlopened with RTLD_GLOBAL.
+          # Fiddle.dlopen(path) is same as Fiddle::Handle.new(path),
+          # and Fiddle::Handle.new defaultly specifies RTLD_GLOBAL.
+          Fiddle.dlopen(path).tap do |handle|
+            debug_report "dlopen(#{path.inspect}) = #{handle.inspect}" if handle
+          end
+        end
+
+        def debug_report(message)
+          return unless debug?
+          $stderr.puts "DEBUG(find_libjulia) #{message}"
+        end
+
+        def debug?
+          @debug_p ||= (ENV['RUBY_JULIA_DEBUG_FIND_LIBJULIA'] == '1')
+        end
       end
     end
   end

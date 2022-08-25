89e383006756e9a01330503719014b60fc4af3c3
Kenta Murata <mrkn@mrkn.jp>
2022-08-25 22:07:06 +0900

    Change the way to stringify Julia error

--- ext/julia/julia.c
+++ ext/julia/julia.c
@@ -5,24 +5,17 @@ VALUE rbjl_mJulia;
 void
 rbjl_check_julia_exception(const char *message)
 {
-  jl_function_t *sprint;
-  jl_function_t *showerror;
-
   jl_value_t *error = JULIA_API(jl_exception_occurred)();
   if (!error) return;
 
-#if 1
-  jl_value_t *backtrace = JULIA_API(jl_get_backtrace)();
-  jl_function_t *println = (jl_function_t *)JULIA_API(jl_eval_string)("println");
-  JULIA_API(jl_call1)(println, backtrace);
-#endif
+  jl_value_t *backtrace = JULIA_API(jl_eval_string)("stacktrace(catch_backtrace(), true)");
 
   JULIA_API(jl_exception_clear)();
 
-  sprint = (jl_function_t *)JULIA_API(jl_eval_string)("sprint");
-  showerror = (jl_function_t *)JULIA_API(jl_eval_string)("showerror");
+  jl_value_t *showerror = JULIA_API(jl_eval_string)("showerror");
+  jl_function_t *sprint = (jl_function_t *)JULIA_API(jl_eval_string)("sprint");
 
-  jl_value_t *res = JULIA_API(jl_call2)(sprint, showerror, error);
+  jl_value_t *res = JULIA_API(jl_call3)(sprint, showerror, error, backtrace);
   if (res && jl_is_string(res)) {
     const char * c_str = JULIA_API(jl_string_ptr)(res);
     rb_raise(rb_eRuntimeError, "JuliaError: %s (%s)", c_str, message);
